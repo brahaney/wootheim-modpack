0229461c5601622c7d84b492eda858c8b708febe
Daniel Brahaney <4593316+brahaney@users.noreply.github.com>
2021-03-05 11:24:37 -0500

    updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,4 +2,4 @@
 read -p "Enter Version Tag: " tag
 (cd client && zip -r ../dist/client-$tag.zip ./**)
 (cd server && zip -r ../dist/server-$tag.zip ./**)
-gh release create $tag ./dist/*-$tag.zip
+gh release create $tag ./dist/*-$tag.zip -t "$tag"
